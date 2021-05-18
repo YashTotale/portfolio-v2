bb3b56705e944b21dd7d78843689cf3466a04afd
Yash Totale <totaleyash@gmail.com>
2021-05-18 14:18:59 -0700

    Refactored Experience Contents

---
+++ src/Pages/Experience/Contents.tsx
@@ -0,0 +1,70 @@
+// React Imports
+import React, { FC, useCallback, useMemo } from "react";
+import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer";
+import { Document } from "@contentful/rich-text-types";
+import SingleExperience from "./SingleExperience";
+import { ExperienceFields } from "../../Utils/types";
+
+// Redux Imports
+import { useSelector } from "react-redux";
+import { getExperienceSearch } from "../../Redux";
+
+// Material UI Imports
+import { Typography } from "@material-ui/core";
+
+interface ContentsProps {
+  experience: ExperienceFields[];
+}
+
+const Contents: FC<ContentsProps> = ({ experience }) => {
+  const search = useSelector(getExperienceSearch);
+  const normalizedSearch = search.toLowerCase();
+
+  const getExperienceMatch = useCallback(
+    (e: ExperienceFields) => {
+      const matches: boolean[] = [
+        e.title.toLowerCase().includes(normalizedSearch),
+        documentToPlainTextString(e.description as Document)
+          .toLowerCase()
+          .includes(normalizedSearch),
+        documentToPlainTextString(e.responsibilities as Document)
+          .toLowerCase()
+          .includes(normalizedSearch),
+        e.type.toLowerCase().includes(normalizedSearch),
+        e.role.toLowerCase().includes(normalizedSearch),
+        e.start.toLowerCase().includes(normalizedSearch),
+        e.end?.toLowerCase().includes(normalizedSearch) ?? false,
+        e.link?.toLowerCase().includes(normalizedSearch) ?? false,
+        e.github?.toLowerCase().includes(normalizedSearch) ?? false,
+      ];
+
+      return matches;
+    },
+    [normalizedSearch]
+  );
+
+  const filteredExperience = useMemo(() => {
+    if (!normalizedSearch.length) return experience;
+
+    return experience.reduce((arr, exp) => {
+      const matches = getExperienceMatch(exp);
+
+      if (matches.some((bool) => bool)) return [...arr, exp];
+
+      return arr;
+    }, [] as ExperienceFields[]);
+  }, [experience, normalizedSearch, getExperienceMatch]);
+
+  if (!filteredExperience.length)
+    return <Typography variant="h6">No experience found</Typography>;
+
+  return (
+    <>
+      {filteredExperience.map((fields) => (
+        <SingleExperience key={fields.id} {...fields} />
+      ))}
+    </>
+  );
+};
+
+export default Contents;
--- src/Pages/Experience/index.tsx
+++ src/Pages/Experience/index.tsx
@@ -1,11 +1,8 @@
 // React Imports
-import React, { FC, useCallback, useMemo } from "react";
-import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer";
-import { Document } from "@contentful/rich-text-types";
-import SingleExperience from "./SingleExperience";
+import React, { FC } from "react";
+import Contents from "./Contents";
 import Filters from "../../Components/Filters";
 import { useExperience } from "../../Context/DataContext";
-import { ExperienceFields } from "../../Utils/types";
 
 // Redux Imports
 import { useSelector } from "react-redux";
@@ -13,7 +10,7 @@ import { getExperienceSearch, setExperienceSearch } from "../../Redux";
 import { useAppDispatch } from "../../Store";
 
 // Material UI Imports
-import { CircularProgress, makeStyles, Typography } from "@material-ui/core";
+import { CircularProgress, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -57,61 +54,6 @@ const Container: FC = ({ children }) => {
   const classes = useStyles();
 
   return <div className={classes.container}>{children}</div>;
-};
-
-interface ContentsProps {
-  experience: ExperienceFields[];
-}
-
-const Contents: FC<ContentsProps> = ({ experience }) => {
-  const search = useSelector(getExperienceSearch);
-  const normalizedSearch = search.toLowerCase();
-
-  const getExperienceMatch = useCallback(
-    (e: ExperienceFields) => {
-      const matches: boolean[] = [
-        e.title.toLowerCase().includes(normalizedSearch),
-        documentToPlainTextString(e.description as Document)
-          .toLowerCase()
-          .includes(normalizedSearch),
-        documentToPlainTextString(e.responsibilities as Document)
-          .toLowerCase()
-          .includes(normalizedSearch),
-        e.type.toLowerCase().includes(normalizedSearch),
-        e.role.toLowerCase().includes(normalizedSearch),
-        e.start.toLowerCase().includes(normalizedSearch),
-        e.end?.toLowerCase().includes(normalizedSearch) ?? false,
-        e.link?.toLowerCase().includes(normalizedSearch) ?? false,
-        e.github?.toLowerCase().includes(normalizedSearch) ?? false,
-      ];
-
-      return matches;
-    },
-    [normalizedSearch]
-  );
-
-  const filteredExperience = useMemo(() => {
-    if (!normalizedSearch.length) return experience;
-
-    return experience.reduce((arr, exp) => {
-      const matches = getExperienceMatch(exp);
-
-      if (matches.some((bool) => bool)) return [...arr, exp];
-
-      return arr;
-    }, [] as ExperienceFields[]);
-  }, [experience, normalizedSearch, getExperienceMatch]);
-
-  if (!filteredExperience.length)
-    return <Typography variant="h6">No experience found</Typography>;
-
-  return (
-    <>
-      {filteredExperience.map((fields) => (
-        <SingleExperience key={fields.id} {...fields} />
-      ))}
-    </>
-  );
 };
 
 export default Experience;
