7cddeb2f624df8049d3e71ec8a10288d749834ce
Yash Totale <totaleyash@gmail.com>
2021-06-10 18:27:27 -0700

    Refactored Home Page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,7 @@
 //React Imports
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { Document } from "@contentful/rich-text-types";
-import Typer from "./Typer";
+import Typist from "react-typist";
 import RichText from "../../Components/RichText";
 
 // Data Imports
@@ -19,9 +19,6 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: "95%",
     margin: "auto",
   },
-  typist: {
-    display: "inline-block",
-  },
 }));
 
 const Home: FC = () => {
@@ -33,6 +30,49 @@ const Home: FC = () => {
       <Typer />
       <RichText richText={main.description as Document} variant="body1" />
     </div>
+  );
+};
+
+const useTyperStyles = makeStyles((theme) => ({
+  typist: {
+    display: "inline-block",
+  },
+}));
+
+const Typer: FC = () => {
+  const PREFIX = "Check out my ";
+  const classes = useTyperStyles();
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    setCount(1);
+  }, [count]);
+
+  return (
+    <Typography variant="h3">
+      {count ? (
+        <>
+          {PREFIX}
+          <Typist
+            className={classes.typist}
+            avgTypingDelay={50}
+            onTypingDone={() => setCount(0)}
+            cursor={{
+              blink: true,
+            }}
+          >
+            <span>projects</span>
+            <Typist.Backspace count={"projects".length} delay={2000} />
+            <span>experience</span>
+            <Typist.Backspace count={"experience".length} delay={2000} />
+            <span>articles</span>
+            <Typist.Backspace count={"articles".length} delay={2000} />
+          </Typist>
+        </>
+      ) : (
+        PREFIX
+      )}
+    </Typography>
   );
 };
 
