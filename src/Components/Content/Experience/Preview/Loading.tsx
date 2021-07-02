d5d3fa85f8b680e8eebecf987bf785e1b5640c4a
Yash Totale <totaleyash@gmail.com>
2021-07-01 21:21:19 -0700

    Lazy Loaded Experience Preview

---
+++ src/Components/Content/Experience/Preview/Loading.tsx
@@ -0,0 +1,32 @@
+// React Imports
+import React, { FC } from "react";
+import clsx from "clsx";
+
+// Material UI Imports
+import { makeStyles } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
+
+const useStyles = makeStyles((theme) => ({
+  skeleton: {
+    margin: theme.spacing(2, 0),
+    height: 700,
+    width: "100%",
+  },
+}));
+
+interface LoadingProps {
+  className?: string;
+}
+
+const Loading: FC<LoadingProps> = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Skeleton
+      variant="rect"
+      className={clsx(classes.skeleton, props.className)}
+    />
+  );
+};
+
+export default Loading;
