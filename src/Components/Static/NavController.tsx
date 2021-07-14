d0c24972b9affab1d12e393ca47fd2e441c56a3e
Yash Totale <totaleyash@gmail.com>
2021-07-14 10:50:49 -0700

    Added Scroll To Top Button

--- src/Components/Static/NavController.tsx
+++ src/Components/Static/NavController.tsx
@@ -1,6 +1,5 @@
 // React Imports
-import React, { FC, useEffect } from "react";
-import { useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { Redirect, useLocation } from "react-router";
 import { useTitle } from "../../Context/HeadContext";
 
@@ -8,11 +7,16 @@ import { useTitle } from "../../Context/HeadContext";
 import { addToHistory, modifyLastHistory } from "../../Redux";
 import { useAppDispatch } from "../../Store";
 
+// Material UI Imports
+import { Fab, makeStyles, useScrollTrigger, Zoom } from "@material-ui/core";
+import { KeyboardArrowUp } from "@material-ui/icons";
+
 const NavController: FC = () => {
   const dispatch = useAppDispatch();
   const [prevPath, setPrevPath] = useState<string | null>(null);
 
-  const pathname = useLocation().pathname;
+  const location = useLocation();
+  const pathname = location.pathname;
   const title = useTitle();
 
   const withTrailingSlash = pathname.charAt(pathname.length - 1) === "/";
@@ -34,9 +38,44 @@ const NavController: FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [normalizedPath, title, dispatch]);
 
-  if (withTrailingSlash) return <Redirect to={pathname.slice(0, -1)} />;
+  if (withTrailingSlash)
+    return <Redirect to={{ ...location, pathname: pathname.slice(0, -1) }} />;
 
-  return null;
+  return <ScrollToTop />;
+};
+
+const useScrollToTopStyles = makeStyles((theme) => ({
+  scrollToTop: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+const ScrollToTop: FC = () => {
+  const classes = useScrollToTopStyles();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const onClick = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div className={classes.scrollToTop} onClick={onClick}>
+        <Fab color="secondary" size="small" title="Scroll to Top">
+          <KeyboardArrowUp />
+        </Fab>
+      </div>
+    </Zoom>
+  );
 };
 
 export default NavController;
